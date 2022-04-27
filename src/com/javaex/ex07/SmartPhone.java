8dd85bc058b0dba1fd76207d292be5a4fc0b1cc5
MarshmallowShadow <choiboseung@naver.com>
2022-04-28 07:54:50 +0900

    2022-04-28

---
+++ src/com/javaex/ex07/SmartPhone.java
@@ -0,0 +1,26 @@
+package com.javaex.ex07;
+
+public class SmartPhone extends MusicPhone {
+    
+    public void execute(String str){
+        
+        //코드작성
+        if("앱".equals(str)) {
+        	launchApp();
+        } else {
+        	super.execute(str);
+        }
+    }
+ 
+    //메소드작성
+    public void launchApp() {
+    	System.out.println("앱실행");
+    }
+    
+    //메소드작성
+    protected void playMusic(){
+        System.out.println("다운로드해서 음악재생");
+    }
+    
+    
+}
